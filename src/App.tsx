d0a8a15762a9b82f9a5aaf79d27c708475d3f626
zor07 <anzor.karmov@gmail.com>
2023-04-23 19:09:35 +0300

    fix moved App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+AppRegistry.registerComponent('main', () => App);
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
